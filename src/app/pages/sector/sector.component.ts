0ebc603f2467e38c26bf4ae26dcb8f72f0de52b7
gabe239 <gurbinaia@gmail.com>
2024-05-31 15:14:34 -0400

    sector component

---
+++ src/app/pages/sector/sector.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sector',
+  standalone: true,
+  imports: [],
+  templateUrl: './sector.component.html',
+  styleUrl: './sector.component.css'
+})
+export class SectorComponent {
+
+}

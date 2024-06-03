362f77c125cb6ef061cc12becfd0f6a799f7141c
jc0095 <110313425+jc0095@users.noreply.github.com>
2024-06-02 23:28:40 -0400

    rutas
    rutas agregadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/partials/header/header.component";
 import { SectorComponent} from "./pages/buy-tickets/sector/sector.component";
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, SectorComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, SectorComponent,LoginComponent,RegistroComponent]
 })
 export class AppComponent {
   title = 'biticket-front';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,14 @@ export const routes: Routes = [
         loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
     },
     {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent),
+    },
+    {
+        path: 'registro',
+        loadComponent: () => import('./pages/registro/registro.component').then(m => m.RegistroComponent),
+    },
+    {
         path: 'buy',
         loadChildren: () => import('./pages/buy-tickets/buy-tickets.routes').then(m => m.routes),
     },
